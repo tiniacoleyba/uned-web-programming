9a42b772616fe232f183237b07ce50f58702dab4
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-05-02 11:38:13 +0100

    Developed basic register's form validation method.

--- dir_javascript/interactivo.js
+++ dir_javascript/interactivo.js
@@ -1,5 +1,8 @@
-const validador = {
+const Validador = {
+    formatoClave : "La contraseña debe tener como mínimo 8 caracteres y" +
+        "contener una mayúscula y un número.",
     validarClave : function(clave) {
+        // TODO: implementar comprobacion de formato
         return clave.trim().length>7;
     },
     validarCorreo : function(correo) {
@@ -17,16 +20,16 @@ const validador = {
     validarAcceso : function() {
         const clave  = document.getElementById("pcont");
         const correo = document.getElementById("pcorreo");
-        const esCorreoValido = validador.validarCorreo(correo.value);
-        const esClaveValida = validador.validarClave(clave.value);
+        const esCorreoValido = Validador.validarCorreo(correo.value);
+        const esClaveValida = Validador.validarClave(clave.value);
 
-        if(!esCorreoValido) {
+        if(!esCorreoValido){
             correo.focus();
             window.alert('Correo inválido.');
             return false;
-        } else if (!esClaveValida) {
+        }else if(!esClaveValida){
             clave.focus();
-            window.alert('Clave inválida, debe ser mayor de 7 caracteres.');
+            window.alert(Validador.formatoClave);
             return false;
         }
         window.alert("Indentificado correctamente.")
@@ -36,7 +39,7 @@ const validador = {
         return ids.map(id=>document.getElementById(id).checked)
             .reduce((a,b)=> a || b);
     },
-    cualBotonSelecionado : function(ids) {
+    queBotonSelecionado : function(ids) {
         return ids.map(id=>document.getElementById(id))
             .filter(id=>id.checked)[0];
     },
@@ -47,20 +50,20 @@ const validador = {
         const aposicion = document.getElementById("aposicionid");
         const imgtam    = document.getElementById("imgtam");
         const textarea  = document.getElementById("textareaid");
-        const tipos_anuncio = ["tanuncio_t","tanuncio_i"];
-        const modos_anuncio = ["manuncio_s","manuncio_p"];
-        const tipoSelec = validador.validarBotonesRadio(tipos_anuncio);
-        const modoSelec = validador.validarBotonesRadio(modos_anuncio);
+        const tiposAnuncio = ["tanuncio_t","tanuncio_i"];
+        const modoAnuncio = ["manuncio_s","manuncio_p"];
+        const tipoSelec = Validador.validarBotonesRadio(tiposAnuncio);
+        const modoSelec = Validador.validarBotonesRadio(modoAnuncio);
 
         if(nombre.value.trim().length == 0) {
             nombre.focus();
             window.alert('Debe rellenar el campo nombre.');
             return false;
-        }else if(!validador.validarCif(ecif.value)) {
+        }else if(!Validador.validarCif(ecif.value)) {
             ecif.focus();
             window.alert('Formato de CIF inválido.')
             return false;
-        }else if(!validador.validarCorreo(correo.value)) {
+        }else if(!Validador.validarCorreo(correo.value)) {
             correo.focus();
             window.alert('Formato de correo inválido.');
             return false;
@@ -73,17 +76,66 @@ const validador = {
         }else if(aposicionid.value === "") {
             window.alert('Debe elegir la posición del anuncio.');
             return false;
-        }else if(validador.cualBotonSelecionado(tipos_anuncio).value == 'imagen'
+        }else if(Validador.queBotonSelecionado(tiposAnuncio).value == 'imagen'
                 && imgtam.value === "") {
             window.alert('Debe elegir el tamaño de la imagen.');
             return false;
-        }else if(validador.cualBotonSelecionado(tipos_anuncio).value == 'texto'
+        }else if(Validador.queBotonSelecionado(tiposAnuncio).value == 'texto'
                 && textarea.value.trim() === "") {
             textarea.focus();
             window.alert('Debe escribir el texto del anuncio.');
             return false;
         }
         window.alert('Su solicitud ha sido enviada. Le contacteremos pronto.')
+        return true;
+    },
+    validarfechaNac : function(fechaNac) {
+        // TODO: implementar comprobación
+        return true;
+    },
+    validarRegistro : function() {
+        const nombre      = document.getElementById("nombre");
+        const apellido    = document.getElementById("apellido");
+        const fechaNac    = document.getElementById("fechanac");
+        const correo      = document.getElementById("correo");
+        const clave       = document.getElementById("pcont");
+        const resRecup    = document.getElementById("rrec");
+        const preguntaRec = document.getElementById("prec");
+        const sexo = ["sfemenino","smasculino"];
+        const generoSelec = Validador.validarBotonesRadio(sexo);
+
+        if(nombre.value.trim().length == 0){
+            nombre.focus();
+            window.alert('Debe rellenar el campo nombre.');
+            return false;
+        }else if(apellido.value.trim().length == 0){
+            apellido.focus();
+            window.alert('Debe rellenar el campo apellido.');
+            return false;
+        }else if(!Validador.validarfechaNac(fechaNac.value)){
+            fechaNac.focus();
+            window.alert('El formato de fecha debe ser dd/mm/aaaa.');
+            return false;
+        }else if(Validador.validarBotonesRadio(sexo) == ""){
+            window.alert('Debe elegir su género.');
+            return false;
+        }else if(!Validador.validarCorreo(correo.value)){
+            correo.focus();
+            window.alert('Debe introducir un correo válido.');
+            return false;
+        }else if(!Validador.validarClave(clave.value)){
+            clave.focus();
+            window.alert(Validador.formatoClave);
+            return false;
+        }else if(preguntaRec.value == ""){
+            window.alert('Debe elegir la pregunta de recuperación.');
+            return false;
+        }else if(rrec.value.trim().length == 0){
+            rrec.focus();
+            window.alert("Debe rellenar la respuesta de recuperación.");
+            return false;
+        }
+        window.alert("Se ha registrado correctamente.");
         return true;
     }
 };
